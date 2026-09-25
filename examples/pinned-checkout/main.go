5329d9f8ae297ffeb7ea50a388136f4a9dba7957
Hec <me@xynova.com>
2026-09-25 21:15:20 +1000

    chore: add LICENSE, pinned-checkout example, and lint gate (#1)

---
+++ examples/pinned-checkout/main.go
@@ -0,0 +1,32 @@
+// Package main is a compile-tested example of pinned checkout usage.
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/behaviorengineering/majordomo-forge-clients/pkg/auth"
+	"github.com/behaviorengineering/majordomo-forge-clients/pkg/gitclone"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Fprintln(os.Stderr, "usage: example <remote-url> <branch> <dst>")
+		os.Exit(2)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+	client := &gitclone.Client{Cred: auth.Credential{Token: os.Getenv("FORGE_TOKEN"), SCM: auth.InferSCM(os.Args[1])}}
+	sha, err := client.ResolveBranchHead(ctx, os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if err := client.EnsureCheckout(ctx, os.Args[1], os.Args[3], sha); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(sha)
+}
